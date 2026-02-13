f2b494dec830992f3e6ee43e0af2ccc8c289f210
ikwanshafiq <ikwanshafiq.official@gmail.com>
2026-02-13 11:17:09 +0800

    fix: update greeting name in greet function

--- greet.py
+++ greet.py
@@ -2,5 +2,5 @@ def greet(name):
     return f"""Hello, my name is test
     {name}!"""  # f-strings = like C# interpolated strings
 
-print(greet("Muhammad Ikwan Shafiq Bin Muhana Bin Abdul Rahim"))
+print(greet("Muhammad Ikwan Shafiq Bin Muhana "))
 
